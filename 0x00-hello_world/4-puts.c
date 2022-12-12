6f8697e53ca99cf455be6dfb9bf2fbb9f8ca2095
Amos Adebajo <amosadebajo@gmail.com>
2022-12-12 15:09:23 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * This file prints a text
+ *     *
+ *      * Return: Always 0 (Success)
+ *       */
+int main(void)
+{
+		char prog_puzz[] = "\"Programming is like building a multilingual puzzle";
+
+			puts(prog_puzz);
+
+				return (0);
+}
