adc395d857ed966e4a6f35e0725ad7242d1ab224
Amos Adebajo <amosadebajo@gmail.com>
2022-12-12 15:48:50 -0800

    task7

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ *  * main - hello
+ *   *
+ *    * This code dances
+ *     *
+ *      * Return: -0 always success
+ *       */
+int main(void)
+{
+		char c;
+			int i;
+				long int li;
+					long long int ll;
+						float f;
+
+							printf("Size of a char: %lu byte\(s)\n", (unsigned long)sizeof(c));
+								printf("Size of an int: %lu byte\(s)\n", (unsigned long)sizeof(i));
+									printf("Size of a long int: %lu byte\(s)\n", (unsigned long)sizeof(li));
+										printf("Size of a long long int: %lu byte\(s)\n", (unsigned long)sizeof(ll));
+											printf("Size of a float: %lu byte\(s)\n", (unsigned long)sizeof(f));
+
+												return (0);
+}
