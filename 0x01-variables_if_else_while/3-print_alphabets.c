4888b86d7281cd448ee7ba191336002e15b871fd
Amos Adebajo <amosadebajo@gmail.com>
2022-12-12 17:08:32 -0800

    task2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - alphabet printing code
+ *
+ *   *
+ *
+ *    * Return: 0 for a sucessful run
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int lowerC = 97;
+
+			int UpperC = 65;
+
+
+
+				while (lowerC < 123)
+
+						{
+
+									putchar(lowerC);
+
+											lowerC++;
+
+												}
+
+
+
+					while (UpperC < 91)
+
+							{
+
+										putchar(UpperC);
+
+												UpperC++;
+
+													}
+
+
+
+						putchar(10);
+
+
+
+							return (0);
+
+}
