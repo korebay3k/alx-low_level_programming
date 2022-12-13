c94b4a8061ba5f2fbf76db61a49410f6dee2ada1
Amos Adebajo <amosadebajo@gmail.com>
2022-12-12 17:16:09 -0800

    task4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - number printing
+ *
+ *   *
+ *
+ *    * Return: 0 for success
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int num = 48;
+
+
+
+			while (num < 58)
+
+					{
+
+								putchar(num);
+
+										num++;
+
+											}
+
+
+
+				putchar(10);
+
+
+
+					return (0);
+
+}
+
+
