f2f61b347067b46583185dfb7d5aa34ac7f71d16
Amos Adebajo <amosadebajo@gmail.com>
2022-12-12 17:11:25 -0800

    task3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - the body of my code
+ *
+ *   *
+ *
+ *    * Return: 0 for sucess
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int lowerC = 97;
+
+
+
+			while (lowerC < 123)
+
+					{
+
+								if (lowerC == 113 || lowerC == 101)
+
+											{
+
+															lowerC++;
+
+																	}
+
+
+
+									putchar(lowerC);
+
+										lowerC++;
+
+											}
+
+
+
+				putchar(10);
+
+
+
+					return (0);
+
+}
