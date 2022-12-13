c1e50bd7e3fd80c58698323fd6254ef496edd751
Amos Adebajo <amosadebajo@gmail.com>
2022-12-12 17:05:58 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - alphabet printing code
+ *
+ *   *
+ *
+ *    * Return: 0 for a sucessful run
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int alphab = 97;
+
+
+
+			while (alphab < 123)
+
+					{
+
+								putchar(alphab);
+
+										alphab++;
+
+											}
+
+
+
+				putchar(10);
+
+
+
+					return (0);
+
+}
+
+
