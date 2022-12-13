1ff19ed6c964ba1cab6efe6709a6fd14b80c0bfe
Amos Adebajo <amosadebajo@gmail.com>
2022-12-12 16:00:48 -0800

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,35 @@
+#include<unistd.h>
+
+
+
+/**
+ *
+ *  * main - the body
+ *
+ *   *
+ *
+ *    * We use a different type of header
+ *
+ *     *
+ *
+ *      * Return: 1 for reasons yet discovered
+ *
+ *       */
+
+int main(void)
+
+{
+
+		char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
+
+			write(2, err, 59);
+
+
+
+				return (1);
+
+}
+
+
