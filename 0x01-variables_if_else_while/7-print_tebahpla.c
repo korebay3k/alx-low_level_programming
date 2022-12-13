a93133648aa0d8c56a30238d907fda63cad6bd1b
Amos Adebajo <amosadebajo@gmail.com>
2022-12-12 17:26:49 -0800

    task 5

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Alphabet in reverse
+ *
+ *   *
+ *
+ *    * Return: 0 for sucessful
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int ralpha = 122;
+
+
+
+			while (ralpha > 96)
+
+					{
+
+								putchar(ralpha);
+
+										ralpha--;
+
+											}
+
+
+
+				putchar(10);
+
+
+
+					return (0);
+
+}
