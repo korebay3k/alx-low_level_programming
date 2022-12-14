6e48f27f0c57b19605ff6e710c10b7b95e87a5ee
Amos Adebajo <amosadebajo@gmail.com>
2022-12-13 19:22:54 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - prints letters
+ *
+ *   * Return: no return
+ *
+ *    */
+
+
+
+void print_alphabet(void)
+
+{
+
+		char c;
+
+
+
+			for (c= 'a'; c <= 'z'; c++)
+
+					{
+
+								_putchar(c);
+
+									}
+
+				_putchar('\n');
+
+}
