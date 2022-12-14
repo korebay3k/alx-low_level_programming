b8a9aae28db5302e54980933ff21cc888f406fae
Amos Adebajo <amosadebajo@gmail.com>
2022-12-13 20:28:30 -0800

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,49 +1,21 @@
 #include "main.h"
 
-	#include <stdio.h>
+/**
+ *
+ *  * _abs - a function that computes the absolute value of an integer
+ *
+ *   * @ab: integer input
+ *
+ *    * Return: absolute value of ab
+ *
+ *     */
 
+int _abs(int ab)
 
+{
 
+		return (ab * ((ab > 0) - (ab < 0)));
 
-
-	/**
-	 *
-	 * 	 * main - check the code
-	 *
-	 * 	 	 *
-	 *
-	 * 	 	 	 * Return: Always 0.
-	 *
-	 * 	 	 	 	 */
-
-	int main(void)
-
-		{
-
-				    int r;
-
-
-
-
-
-				    	    r = _abs(-1);
-
-					    	    printf("%d\n", r);
-
-						    	    r = _abs(0);
-
-							    	    printf("%d\n", r);
-
-								    	    r = _abs(1);
-
-									    	    printf("%d\n", r);
-
-										    	    r = _abs(-98);
-
-											    	    printf("%d\n", r);
-
-												    	    return (0);
-
-													    	}
+}
 
 
--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,4 +1,6 @@
-/**
+#include "main.h"
+#include <stdio.h>
+/** 
  *
  *  * main - check the code
  *
@@ -14,24 +16,13 @@ int main(void)
 
 	    int r;
 
-
-
 	        r = _abs(-1);
-
-		    printf("%d\n", r);
-
-		        r = _abs(0);
-
-			    printf("%d\n", r);
-
-			        r = _abs(1);
-
-				    printf("%d\n", r);
-
-				        r = _abs(-98);
-
-					    printf("%d\n", r);
-
-					        return (0);
-
+                printf("%d\n", r);
+	        r = _abs(0);
+                printf("%d\n", r);
+	        r = _abs(1);
+                printf("%d\n", r);
+	        r = _abs(-98);
+		printf("%d\n", r);
+		return (0);
 }
