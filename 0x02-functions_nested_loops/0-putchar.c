9ecb0901b7550b0fba619c6e01fbc73dac2c832a
Amos Adebajo <amosadebajo@gmail.com>
2022-12-13 19:01:38 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - This program uses only putchar with ascii values
+ *
+ *   *
+ *
+ *    * Return: 0 for success
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		_putchar('_');
+
+			_putchar('p');
+
+				_putchar('u');
+
+					_putchar('t');
+
+						_putchar('c');
+
+							_putchar('h');
+
+								_putchar('a');
+
+									_putchar('r');
+
+										_putchar('\n');
+
+
+
+											return (0);
+
+}
