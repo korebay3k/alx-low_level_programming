7745ec98884b3845dd963d95edba04810ed2f667
Amos Adebajo <amosadebajo@gmail.com>
2022-12-19 20:37:29 -0800

    last task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *  * main - password gen for crackme
+ *   *
+ *    * Return: 0 always
+ *     */
+int main(void)
+{
+		int pass[100];
+			int i, sum, n;
+
+				sum = 0;
+
+					srand(time(NULL));
+
+						for (i = 0; i < 100; i++)
+								{
+											pass[i] = rand() % 78;
+													sum += (pass[i] + '0');
+															putchar(pass[i] + '0');
+																	if ((2772 - sum) - '0' < 78)
+																				{
+																								n = 2772 - sum - '0';
+																											sum += n;
+																														putchar(n + '0');
+																																	break;
+																																			}
+
+																		}
+
+							return (0);
+}
