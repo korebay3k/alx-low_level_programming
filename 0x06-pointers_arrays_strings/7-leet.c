56c21d327ec95ae0a2e840c16d8ec40bbdbb15a3
Amos Adebajo <amosadebajo@gmail.com>
2022-12-23 19:39:39 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,28 @@
 #include "main.h"
+
 /**
- *  * leet - encode into 1337speak
- *   * @n: input value
- *    * Return: n value
- *     */
+ *  * leet - Entry point
+ *   * ONE if, TWO loops only...
+ *    * @n: input
+ *     * Return: Always 0 (Success)
+ *      */
 char *leet(char *n)
 {
-		int i, j;
-			char s1[] = "aAeEoOtTlL";
-				char s2[] = "4433007711";
+		int i, x;
+			int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+				int replacer[] = {'4', '3', '0', '7', '1'};
 
 					for (i = 0; n[i] != '\0'; i++)
 							{
-										for (j = 0; j < 10; j++)
+										for (x = 0; x <= 9; x++)
 													{
-																	if (n[i] == s1[j])
+																	if (n[i] == find[x])
 																					{
-																										n[i] = s2[j];
-																													}
+																										n[i] = replacer[x / 2];
+																														x = 9;
+																																	}
 																			}
 											}
+
 						return (n);
 }
-
