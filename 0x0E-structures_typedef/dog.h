499229f1ae42144256fbdde0c7f32e80760bbbe8
Akorede Adebajo <korebay3k@yahoo.com>
2023-01-17 20:53:02 -0800

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,14 @@
-#ifndef DOGH_
-#define DOGH_
-
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- *  * struct dog - creates a dogtag
- *   * @name: name of doggo
- *    * @age: age of doggo
- *     * @owner: of doggo
+ *  * struct dog - a dog's basic info
+ *   * @name: First member
+ *    * @age: Second member
+ *     * @owner: Third member
  *      *
- *       */
+ *       * Description: Longer description
+ *        */
 struct dog
 {
 		char *name;
@@ -20,13 +17,15 @@ struct dog
 };
 
 /**
- *  * dog_t - dogtype struct
+ *  * dog_t - typedef for struct dog
  *   */
 typedef struct dog dog_t;
 
-void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
